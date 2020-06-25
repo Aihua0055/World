67b347d889c5e412e1adbbcd9f63bca22d40d7b5
Aihua Chen <aihua@pdx.edu>
2020-06-24 23:37:46 -0700

    finished HDI  data visualization , cleaned up code

--- explore_the_world/hdi.js
+++ explore_the_world/hdi.js
@@ -1,30 +1,24 @@
 var years = [1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018];
-var asian = ['Iran','Iraq','Israel','Nepal','Malaysia','Thailand','Turkey','Vietnam','Japan','Singapore','Hong Kong, China (SAR)','Korea (Republic of)','India'];
-
-var other = ['Australia','New Zealand','Egypt','Canada','Cuba','Mexico'];
-var europe = ['Austria','Norway','Switzerland','Ireland','Germany','Iceland','Sweden','Denmark','Finland','United Kingdom','Spain','France','Italy','Greece']
 var chinaHdi = [0.501,0.509,0.520,0.530,0.537,0.549,0.558,0.566,0.574,0.583,0.591,0.599,0.610,0.622,0.631,0.643,0.657,0.670,0.681,0.690,0.702,0.711,0.719,0.727,0.735,0.742,0.749,0.753,0.758 ];
 var usHdi = [0.860,0.862,0.867,	0.872,	0.875,	0.878,	0.879,0.881,	0.884,	0.885,	0.881,	0.884,	0.886,	0.889,	0.892,	0.896,	0.899,	0.902,	0.907,	0.908,	0.911,	0.914,	0.916,	0.914,	0.915,	0.917,	0.919,	0.919,	0.920];
 
-// TODO: add "Loading Data"
+// Show HDI of China and the U.S. as default
 var series = [
   {
   name:'No.85 China',
   data:chinaHdi,
-
 },
 {
   name: 'No.15 United States',
   data: usHdi
 }
 ];
-
+// Options for the apexcharts - line chart
 var options = {
   chart: {
     type: 'line',
     id:"chart"
   },
-
   legend: {
     horizontalAlign: 'center',
     fontSize:'13px',
@@ -44,16 +38,47 @@ var options = {
     },
   },
   subtitle:{
-    text:'Including most European, Asian, North America countries',
+    text:'This chart only show TOP50 2018 HDI Ranking countries and China as well as baseline data for comparision.',
     style:{
       fontSize:'14px',
       color:'#9699a2'
     }
   },
+  noData: {
+    text: 'Loading...'
+  },
+  // Responsive line chart - legend position: right, bottom; legend: show or not
+  responsive: [
+    {
+      breakpoint: 1200,
+      options: {
+
+        legend: {
+          position: "bottom"
+        }
+      }
+    },
+    {
+      breakpoint: 992,
+      options: {
+        legend: {
+          position:"bottom"
+        }
+      }
+    },
+    {
+      breakpoint: 768,
+      options: {
+        legend: {
+          show:false,
+        }
+      }
+    }
+  ],
+
   stroke:{
     width:2,
   },
-
   series: series,
   xaxis: {
     categories: years,
@@ -67,17 +92,17 @@ var options = {
       text:"Years",
       style:{
         color:'black',
-        fontSize:'14PX'
+        fontSize:'12PX'
       },
     },
   },
   yaxis: {
     categories: years,
     title:{
-      text:"HDI Index",
+      text:"HDI Index Value",
       style:{
         color:'black',
-        fontSize:'14PX'
+        fontSize:'12PX'
       },
     },
     labels:{
@@ -87,15 +112,12 @@ var options = {
     }
   }
 }
+
+
 var chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
-
-var barChartData = [];
-var barChartCountry = [];
-
 async function loadData(file){
   d3.csv(file,function(d){
-
     return{
       country: d["Country"],
       hdiRank: +d["HDI Rank (2018)"],
@@ -105,124 +127,540 @@ async function loadData(file){
              +d["2010"],+d["2011"],+d["2012"],+d["2013"],+d["2014"],+d["2015"],+d["2016"],+d["2017"],+d["2018"]
             ]
     }
-
   }).then(function(data){
 
-     
      for(const hdi in data){
        const rank = data[hdi]['hdiRank']
        let element = {};
        element.name= data[hdi]["country"];
        element.data= data[hdi]["data"];
-
        if(rank == 0 ){
         chart.appendSeries(element);
-        // barChartData.push(data[hdi]["data"][29])
-        // barChartCountry.push(element.name);
        }
-       if(rank >0 && rank <85){
+       if(rank >0 && rank <51){
         element.name = 'No.' + rank +' \xa0'+ data[hdi]["country"];
         chart.appendSeries(element);
        }
-       
       //  if (europe.includes(element.name)){
       //   element.name = data[hdi]["country"]+ ' No.' + rank;
       //   chart.appendSeries(element);
       //  }
-       
-      //  if(asian.includes(element.name)){
-      //    element.name = data[hdi]["country"]+ ' No.' + rank;
-      //    chart.appendSeries(element);
-        
-      //  }
-
-      //  if(other.includes(element.name)){
-      //    element.name = data[hdi]["country"]+ ' No.' + rank;
-      //    chart.appendSeries(element);
-         
-      //  }
-
      }
-
      for(const hdi in data){
-
       const rank = data[hdi]['hdiRank']
       let country = data[hdi]["country"]
       if (rank ==0){
         chart.toggleSeries(country);
       }
-      if(rank >0 && rank<85 && rank!=8){
+      if(rank >0 && rank<51 && rank!=8){
         name = 'No.' + rank +' \xa0'+ country;
         chart.toggleSeries(name);
       }
-
-    //   if (!defaultSeries.includes(country)){
-    //     name = data[hdi]["country"];
-    //     chart.toggleSeries(name);
-    //    }
-
      }
-
   });
 }
-
 loadData("hdi.csv");
 
 
-var barChartOptions = {
-  series: [{
-  data: barChartData
-}],
-  chart: {
-  type: 'bar',
-  id:'barChart',
-},
-title:{
-  text:"2018 HDI",
-  style:{
-    fontSize:'24px',
-    
-  },
-},
-plotOptions: {
-  bar: {
-    horizontal: true,
-  }
-},
-dataLabels: {
-  enabled: false
-},
-xaxis: {
-  categories: barChartCountry,
- 
-}
-};
 
-// var barChart = new ApexCharts(document.querySelector("#barChart"), barChartOptions);
-// barChart.render();
-
-
-
-// // check if the checkbox has any unchecked item
-// checkLegends()
-
-// function generateSelection(){
-//   // <div class = 'selection'>
-//   // <input type="checkbox" checked onclick="toggleSeries(this)" value="China No.83"> <label for="China"> China No.83</label><br></br>
-
-// }
-
-// function checkLegends() {
-//   var allLegends = document.querySelectorAll(".selection input[type='checkbox']")
-
-//   for(var i = 0; i < allLegends.length; i++) {
-//     if(!allLegends[i].checked) {
-//       chart.toggleSeries(allLegends[i].value)
-//     }
-//   }
-// }
-
-// // toggleSeries accepts a single argument which should match the series name you're trying to toggle
-// function toggleSeries(checkbox) {
-//   chart.toggleSeries(checkbox.value)
-// }
+var placeholder = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+  30,
+  31,
+  32,
+  33,
+  34,
+  35,
+  36,
+  37,
+  38,
+  39,
+  40,
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48,
+  49,
+  50,
+  51,
+  52,
+  53,
+  54,
+  55,
+  56,
+  57,
+  58,
+  59,
+  60,
+  61,
+  62,
+  63,
+  64,
+  65,
+  66,
+  67,
+  68,
+  69,
+  70,
+  71,
+  72,
+  73,
+  74,
+  75,
+  76,
+  77,
+  78,
+  79,
+  80,
+  81,
+  82,
+  83,
+  84,
+  85,
+  86,
+  87,
+  88,
+  89,
+  90,
+  91,
+  92,
+  93,
+  94,
+  95,
+  96,
+  97,
+  98,
+  99,
+  100,
+  101,
+  102,
+  103,
+  104,
+  105,
+  106,
+  107,
+  108,
+  109,
+  110,
+  111,
+  112,
+  113,
+  114,
+  115,
+  116,
+  117,
+  118,
+  119,
+  120,
+  121,
+  122,
+  123,
+  124,
+  125,
+  126,
+  127,
+  128,
+  129,
+  130,
+  131,
+  132,
+  133,
+  134,
+  135,
+  136,
+  137,
+  138,
+  139,
+  140,
+  141,
+  142,
+  143,
+  144,
+  145,
+  146,
+  147,
+  148,
+  149,
+  150,
+  151,
+  152,
+  153,
+  154,
+  155,
+  156,
+  157,
+  158,
+  159,
+  160,
+  161,
+  162,
+  163,
+  164,
+  165,
+  166,
+  167,
+  168,
+  169,
+  170,
+  171,
+  172,
+  173,
+  174,
+  175,
+  176,
+  177,
+  178,
+  179,
+  180,
+  181,
+  182,
+  183,
+  184,
+  185,
+  186,
+  187,
+  188,
+  189,
+  190,
+  191,
+  192,
+  193,
+  194,
+  195,
+  196,
+  197,
+  198,
+  199,
+  200,
+  201,
+  202,
+  203,
+  204,
+  205,
+  206,
+  207,
+  208,
+  209,
+  210,
+  211,
+  212,
+  213,
+  214,
+  215,
+  216,
+  217,
+  218,
+  219,
+  220,
+  221,
+  222,
+  223,
+  224,
+  225,
+  226,
+  227,
+  228,
+  229,
+  230,
+  231,
+  232,
+  233,
+  234,
+  235,
+  236,
+  237,
+  238,
+  239,
+  240,
+  241,
+  242,
+  243,
+  244,
+  245,
+  246,
+  247,
+  248,
+  249,
+  250,
+  251,
+  252,
+  253,
+  254,
+  255,
+  256,
+  257,
+  258,
+  259,
+  260,
+  261,
+  262,
+  263,
+  264,
+  265,
+  266,
+  267,
+  268,
+  269,
+  270,
+  271,
+  272,
+  273,
+  274,
+  275,
+  276,
+  277,
+  278,
+  279,
+  280,
+  281,
+  282,
+  283,
+  284,
+  285,
+  286,
+  287,
+  288,
+  289,
+  290,
+  291,
+  292,
+  293,
+  294,
+  295,
+  296,
+  297,
+  298,
+  299,
+  300,
+  301,
+  302,
+  303,
+  304,
+  305,
+  306,
+  307,
+  308,
+  309,
+  310,
+  311,
+  312,
+  313,
+  314,
+  315,
+  316,
+  317,
+  318,
+  319,
+  320,
+  321,
+  322,
+  323,
+  324,
+  325,
+  326,
+  327,
+  328,
+  329,
+  330,
+  331,
+  332,
+  333,
+  334,
+  335,
+  336,
+  337,
+  338,
+  339,
+  340,
+  341,
+  342,
+  343,
+  344,
+  345,
+  346,
+  347,
+  348,
+  349,
+  350,
+  351,
+  352,
+  353,
+  354,
+  355,
+  356,
+  357,
+  358,
+  359,
+  360,
+  361,
+  362,
+  363,
+  364,
+  365,
+  366,
+  367,
+  368,
+  369,
+  370,
+  371,
+  372,
+  373,
+  374,
+  375,
+  376,
+  377,
+  378,
+  379,
+  380,
+  381,
+  382,
+  383,
+  384,
+  385,
+  386,
+  387,
+  388,
+  389,
+  390,
+  391,
+  392,
+  393,
+  394,
+  395,
+  396,
+  397,
+  398,
+  399,
+  400,
+  401,
+  402,
+  403,
+  404,
+  405,
+  406,
+  407,
+  408,
+  409,
+  410,
+  411,
+  412,
+  413,
+  414,
+  415,
+  416,
+  417,
+  418,
+  419,
+  420,
+  421,
+  422,
+  423,
+  424,
+  425,
+  426,
+  427,
+  428,
+  429,
+  430,
+  431,
+  432,
+  433,
+  434,
+  435,
+  436,
+  437,
+  438,
+  439,
+  440,
+  441,
+  442,
+  443,
+  444,
+  445,
+  446,
+  447,
+  448,
+  449,
+  450,
+  451,
+  452,
+  453,
+  454,
+  455,
+  456,
+  457,
+  458,
+  459,
+  460,
+  461,
+  462,
+  463,
+  464,
+  465,
+  466,
+  467,
+  468,
+  469,
+  470,
+  471,
+  472,
+  473,
+  474,
+  475,
+  476,
+  477,
+  478,
+  479,
+  480,
+  481,
+  482,
+  483,
+  484,
+  485,
+  486,
+  487,
+  488,
+  489,
+  490,
+  491,
+  492,
+  493,
+  494,
+  495,
+  496,
+  497,
+  498,
+  499
+]
